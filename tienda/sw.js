4b7ac462f1fffa896c56fd574930fe989be8432c
Gorilla Informa <155343531+GorillaInforma@users.noreply.github.com>
2026-07-18 15:40:03 -0600

    Add files via upload

---
+++ tienda/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'fiadoapp-cache-v1';
+const ARCHIVOS_BASICOS = ['./fiadoapp.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS_BASICOS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia: red primero, y si falla (sin internet), usa lo que haya en caché.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copia = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
